c361a41b97f039c49d9cd7d6688dcd172b8828a6
Luna Claire Rose <luna.c.rose@gmail.com>
2013-07-09 05:13:33 -0700

    main script now parses arguments, goes to the partial subdir, and creates dirs if they don't exist

--- cnmoonmars.sh
+++ cnmoonmars.sh
@@ -3,13 +3,53 @@
 #tell grid engine to use current directory
 #$ -cwd
 
-exec > >(tee output/console-output.txt)
+CMDSTR="$@"
+
+while true; do
+	case "$1" in
+		-startIndex)			STARTINDEX="$2"; shift 2 ;;
+		-endIndex)				ENDINDEX="$2"; shift 2 ;;
+		-gridRes)				GRIDRES="$2"; shift 2 ;;
+		-increment)				INCREMENT="$2"; shift 2 ;;
+		-interval)				INTERVAL="$2"; shift 2 ;;
+		-startIndex)			STARTINDEX="$2"; shift 2 ;;
+		-endIndex)				ENDINDEX="$2"; shift 2 ;;
+		-dataDir)				DATADIR="$2"; shift 2 ;;
+		-statusDir)				STATUSDIR="$2"; shift 2 ;;
+		-outputDir)				OUTPUTDIR="$2"; shift 2 ;;
+		-inputFile)				INPUTFILE="$2"; shift 2 ;;
+		-limitsFile)			LIMITSFILE="$2"; shift 2 ;;
+		-abcdDistFile)			ABCDDISTFILE="$2"; shift 2 ;;
+		-possibleHotspotsFile)	POSSIBLEHOTSPOTSFILE="$2"; shift 2 ;;
+		*)						shift; if [ $# == 0 ]; then break; fi
+	esac
+done
+
+CONSOLE_OUT_FILE=output/console-output.txt
+if [ ! -d "output" ]; then
+	mkdir "output/"
+fi
+
+if [[ -n "$STARTINDEX" ]] && [[ -n "$ENDINDEX" ]]; then 
+	MIDDIR=`printf %04d-%04d $STARTINDEX $ENDINDEX`
+	CONSOLE_OUT_FILE="output/$MIDDIR/console-output.txt"
+	if [ ! -d "output/$MIDDIR" ]; then
+		mkdir "output/$MIDDIR"
+	fi
+fi
+
+
+exec > >(tee $CONSOLE_OUT_FILE)
 exec 2>&1
 
 echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 echo Running on:
 hostname
+
+echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+echo Copying console output to: \"$CONSOLE_OUT_FILE\"
 
 echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -23,6 +63,6 @@ make
 
 echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-RUNSTR="./CNmoonmars $@"
+RUNSTR="./CNmoonmars $CMDSTR"
 echo Running \"$RUNSTR\":
 time $RUNSTR
